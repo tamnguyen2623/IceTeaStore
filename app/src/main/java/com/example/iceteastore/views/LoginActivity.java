210b6e4c7709d784e626a8fab53cbf62fa7cb248
quocthai <lxlthailxl@gmail.com>
2025-03-25 13:37:14 +0700

    margin top for product management title

--- app/src/main/java/com/example/iceteastore/views/LoginActivity.java
+++ app/src/main/java/com/example/iceteastore/views/LoginActivity.java
@@ -84,18 +84,18 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-//    private void navigateToAdmin() {
-//        Intent intent = new Intent(this, ProductManagementListActivity.class);
-//        startActivity(intent);
-//        finish();
-//    }
-
-    // chuyen sang trang bill admin
     private void navigateToAdmin() {
-        Intent intent = new Intent(this, AdminBillActivity.class);
+        Intent intent = new Intent(this, ProductManagementListActivity.class);
         startActivity(intent);
         finish();
     }
+
+    // chuyen sang trang bill admin
+//    private void navigateToAdmin() {
+//        Intent intent = new Intent(this, AdminBillActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
 
 
     private void navigateToHome() {
